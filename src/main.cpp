2f34e8babaf101624e3461b5ea3892a12d590786
Roger Zanoni <roger.zanoni@openbossa.org>
2016-04-16 09:48:05 -0400

    Back to the minimal working app

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,14 @@
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
-#include <tmx/MapLoader.h>
-#include <tmx/Log.h>
-
-sf::Vector2f getViewMovement(float dt)
-{
-    sf::Vector2f movement;
-
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        movement.x = -1.f;
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        movement.x = 1.f;
-
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-        movement.y = -1.f;
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-        movement.y = 1.f;
-
-    movement = Helpers::Vectors::normalize(movement) * 500.f * dt;
-    return movement;
-}
 
 void handleWindowEvent(sf::RenderWindow& renderWindow)
 {
     sf::Event event;
-    while(renderWindow.pollEvent(event))
-    {
+    while (renderWindow.pollEvent(event)) {
         if (event.type == sf::Event::Closed
             || (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
-        {
             renderWindow.close();
-        }
     }
 }
 
@@ -46,17 +23,9 @@ sf::Text getFpsText()
 int main()
 {
     sf::RenderWindow renderWindow(sf::VideoMode(800u, 600u), "ld35");
-    sf::Text fpsText = getFpsText();
-
-    tmx::Logger::SetLogLevel(tmx::Logger::Info | tmx::Logger::Error);
-
-    tmx::MapLoader ml("3rdparty/dependencies/sfml-tmxloader/maps/");
-    ml.Load("desert.tmx");
 
     sf::Clock deltaClock, frameClock;
-
     const float dt = 0.01f;
-
     float previousUpdateTime = deltaClock.getElapsedTime().asSeconds();
     float accumulator = 0.f;
 
@@ -71,22 +40,9 @@ int main()
 
         sf::Vector2f movement;
         while (accumulator >= dt)
-        {
-            movement = getViewMovement(dt);
             accumulator -= dt;
-        }
-
-        sf::View view = renderWindow.getView();
-        view.move(movement);
-        renderWindow.setView(view);
-
-        float fpsCount = 1.f / frameClock.restart().asSeconds();
-        fpsText.setString("FPS: " + (std::to_string(fpsCount)));
-        fpsText.move(movement);
 
         renderWindow.clear();
-        renderWindow.draw(ml);
-        renderWindow.draw(fpsText);
         renderWindow.display();
     }
 
